e22a5beb1817e867045017fda55e07a8f68506a7
manshad <manshadchangampalli010@gmail.com>
2021-11-26 22:30:23 +0530

    the images is added at the point of cursor and now the link hover the image will display there

--- src/Components/NewGallery/GalleryItems.jsx
+++ src/Components/NewGallery/GalleryItems.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-function GalleryItems({mouseHover,mouseOut,items,text}) {
-    
+function GalleryItems({img,whichOne,mouseHover,mouseOut,items,text}) {
     return (
-        <div onMouseOut={()=>mouseOut(items)} onMouseOver={()=>mouseHover(items)}  className="items" id={items}>
-            <h1>{text}</h1>
+        <div className="items" id={items}>
+            <h1 style={whichOne===items&&img!="isnull"?{transform:"skew(-5deg)",opacity:1}:{opacity:1}} className="original" onMouseLeave={()=>mouseOut(items)} onMouseEnter={()=>mouseHover(items)} data-text={text} >{text}</h1>
+            <h1 style={whichOne===items&&img!="isnull"?{opacity:1,transform:"skew(-5deg)"}:{opacity:0}} className="dummy">{text}</h1>
         </div>
     )
 }
--- src/pages/NewGallery/NewGallery.jsx
+++ src/pages/NewGallery/NewGallery.jsx
@@ -2,6 +2,11 @@ import React, {useEffect, useState} from 'react'
 import {useSelector} from 'react-redux'
 import GalleryItems from '../../Components/NewGallery/GalleryItems'
 import './NewGallery.scss'
+import zebra from '../../Images/Gallery/zebra-blinds.jpg'
+import roman from '../../Images/Gallery/roman-blinds.jpg'
+import icecrush from '../../Images/Gallery/ice-crush.jpg'
+import longcrush from '../../Images/Gallery/long-crush.jpg'
+import bamboo from '../../Images/Gallery/bamboo-curtain.jpeg'
 
 
 function NewGallery() {
@@ -20,47 +25,70 @@ function NewGallery() {
     }
     const mouseOut = (item)=>{
     }
+    var img='';
     if(item==="item1"&&!removeItem){
+        img = zebra
     }
     else if(item==="item2"&&!removeItem){
+        img = roman
     }else if(item==="item3"&&!removeItem){
+        img = icecrush
     }else if(item==="item4"&&!removeItem){
+        img = longcrush
     }else if(item==="item5"&&!removeItem){
+        img = bamboo
     }else{
+        img="isnull"
     }
-    
 
+    
     return (
         // why is the new gallery means the old gallary componet design is not good or not good model
         <div onMouseOver={()=>setRemoveItem(false)} onMouseOut={()=>setRemoveItem(true)} className="new-gallery">
             <div className="container">
                 <GalleryItems
+                 whichOne ={item}
+                 img={img}
                  mouseHover={mouseHover}
                  mouseOut={mouseOut}
                   text="ZEBRA BLINDS" 
                   items="item1"/>
                 <GalleryItems
+                 whichOne ={item}
                  mouseHover={mouseHover}
+                 img={img}
                  mouseOut={mouseOut}
                  text="ROMAN BLINDS" 
                  items="item2"/>
                 <GalleryItems
+                 whichOne ={item}
                  mouseHover={mouseHover}
+                 img={img}
                  mouseOut={mouseOut}
                  text="ICE CRUSH" 
                  items="item3"/>
                 <GalleryItems
+                 whichOne ={item}
                  mouseHover={mouseHover}
+                 img={img}
                  mouseOut={mouseOut}
                  text="LONG CRUSH" 
                  items="item4"/>
                 <GalleryItems
+                 whichOne ={item}
                  mouseHover={mouseHover}
+                 img={img}
                  mouseOut={mouseOut}
                  text="BAMBOO CURTAIN" 
                  items="item5"/>
             </div>
-            <img src="" alt="" className="gallery-cursor" />
+
+            {
+                img!=="isnull"?
+                <img style={{top:`${offsetY}px`,left:`${offsetX}px`}} src={img} alt="" className="gallery-cursor" />:
+                ""
+            }
+           
         </div>
     )
 }
